7c9b3a9ff18f1aa881b3eb4c565c88b305b73003
Leo Borges <leonardo@kelda.com.br>
2016-07-20 11:15:01 -0300

    Ecma 6 design pattern study

--- Model/design_patterns/structural/bridge/Bridge.js
+++ Model/design_patterns/structural/bridge/Bridge.js
@@ -1,66 +1,104 @@
 'use strict';
 
-class Abstraction {
-    constructor() {
+class AbstractShape {
+    constructor(x, y, themeAPI) {
+        this.themeAPI = themeAPI;
+        this.x = x;
+        this.y = y;
+        this.$domElement = $("<div />").appendTo($("body"));
     }
 
-    Operation (){
-        this.imp.OperationImp();
+    applyNewTheme(themeAPI) {
+        this.themeAPI = themeAPI;
+        this.draw();
+    }
+
+    draw () { }
+    resizeByPercentage(pct) { }
+}
+
+
+class CircleShape extends AbstractShape {
+    constructor(radius, x, y, themeAPI) {
+        super(x, y, themeAPI);
+        this.radius = radius;
+    }
+
+    draw () {
+        // Draw the circle shape
+        this.$domElement.css({
+            'border-radius':'100%',
+            position : 'absolute',
+            left : this.x,
+            top: this.y,
+            height : this.radius + 'px',
+            width: this.radius + 'px'
+        });
+
+        this.themeAPI.stylize(this.$domElement);
+    }
+
+    resizeByPercentage(pct) {
+        this.radius *= pct;
+        this.draw();
+    }
+}
+
+class RectShape extends AbstractShape {
+    constructor(w, h, x, y, themeAPI) {
+        super(x, y, themeAPI);
+        this.w = w;
+        this.h = h;
+    }
+
+    draw () {
+        // Draw the rect shape
+        this.$domElement.css({
+            position : 'absolute',
+            left : this.x,
+            top: this.y,
+            height : this.h + 'px',
+            width: this.w + 'px'
+        });
+
+        this.themeAPI.stylize(this.$domElement);
+    }
+
+    resizeByPercentage(pct) {
+        this.radius *= pct;
+        this.draw();
     }
 }
 
 
-class RefinedAbstraction extends Abstraction {
-    constructor() {
-        super()
-        facade.log('RefinedAbstraction created')
-    }
-
-    setImp (imp) {
-        this.imp = imp
-    }
-
+class AbstractThemeAPI {
+    stylize ($el) { }
 }
 
-
-class Implementor {
-    constructor() {
-    }
-
-    OperationImp (){
+class YellowTheme extends AbstractThemeAPI {
+    stylize ($el) {
+        $el.css({'background-color': 'yellow', border : '3px solid black'});
     }
 }
 
-
-class ConcreteImplementorA extends Implementor {
-    constructor() {
-        super()
-        facade.log('ConcreteImplementorA created')
-    }
-
-    OperationImp (){
-        facade.log('ConcreteImplementorA OperationImp')
+class RedTheme extends AbstractThemeAPI {
+    stylize ($el){
+        $el.css({'background-color': 'red', border : '3px dotted blue'});
     }
 }
 
-class ConcreteImplementorB extends Implementor {
-    constructor() {
-        super()
-        facade.log('ConcreteImplementorB created')
-    }
+f = (function init_Bridge() {
+    let shapes = [
+        new CircleShape(150, 1, 3, new YellowTheme()),
+        new CircleShape(200, 500, 7, new RedTheme()),
+        new RectShape(400, 150, 10, 400, new RedTheme())
+    ];
 
-    OperationImp (){
-        facade.log('ConcreteImplementorB OperationImp')
-    }
-}
+    shapes.forEach((shape) => {
+        shape.draw();
+    });
 
-function init_Bridge() {
-    var abstraction = new RefinedAbstraction()
-    var state = Math.floor(Math.random()*2)
-    if(state)
-        abstraction.setImp(new ConcreteImplementorA())
-    else
-        abstraction.setImp(new ConcreteImplementorB())
-
-    abstraction.Operation()
-}
+    setTimeout( function() {
+        shapes[0].resizeByPercentage(1.7);
+    }, 1500);
+})();
